43e9201d168cfd457583b9f5929f498eb229ee37
Mohan <mohammadhashan99@gmail.com>
2023-06-03 21:00:55 -0400

    03 solution w split,reverse,join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (reverse) {
+  let answer = "";
+  if (reverse !== "") {
+    answer = reverse.split("").reverse().join("");
+  }
+  return answer;
 };
 
 // Do not edit below this line
