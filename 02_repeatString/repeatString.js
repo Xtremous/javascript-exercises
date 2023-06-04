6bbb9c42a938739c3a7beb07852cd63e99b26513
Mohan <mohammadhashan99@gmail.com>
2023-06-03 20:26:16 -0400

    02 solution tested positive

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
 const repeatString = function (string, num) {
-  return string.repeat(num);
+  let finalString = "";
+  if (num < 0) {
+    return "ERROR";
+  } else if (string === "" || num === 0) {
+    return finalString;
+  } else {
+    for (let i = 0; i < num; i++) {
+      finalString += string;
+    }
+    return finalString;
+  }
 };
-
+//!things I learned : dont forget to ask, 'return' kills loop, define empty string var: ""
 // Do not edit below this line
 module.exports = repeatString;
