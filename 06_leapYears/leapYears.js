75d4d4ba9e5250bea5c40acfdba2cd4f172c0e2d
Mohan <mohammadhashan99@gmail.com>
2023-06-06 18:22:46 -0400

    06 unless in question hints at ||

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else return false;
 };
+//todo REDO ONLY ONE LINE, hint: it does go low to high comparasion
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
