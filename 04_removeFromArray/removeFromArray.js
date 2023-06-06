329a5b2c0c2e5aa1adc4abf9d6dc22b9e8ceca9e
Mohan <mohammadhashan99@gmail.com>
2023-06-06 15:18:29 -0400

    working solutions for 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+//todo try again, USING BOTH METHODS.
+//!solution 1 using forEach()
+// const removeFromArray = function (array, ...args){
+//  const newArray = [];
+//   array.forEach((element)=>{
+//     if (!args.includes(element)){
+//       newArray.push(element)
+//     }
+//   });
+//   return newArray;
+// }
+//!solution 2 using filter()
+const removeFromArray = function (array, ...arg) {
+  const newArray = array.filter((element) => !arg.includes(element));
+  console.log(newArray);
+  return newArray;
 };
+console.log(removeFromArray([1, 2, 3, 4, "e"], "e", 2));
+
+//* lesson here: spread operator / rest paremeter, includes(), forEach(), filter();
 
 // Do not edit below this line
 module.exports = removeFromArray;
