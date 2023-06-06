520b21ce5a840b18060fa455b0ab51a342ecf59a
Mohan <mohammadhashan99@gmail.com>
2023-06-06 16:52:16 -0400

    05 works with reduce but loop adding works too

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (lowEnd, highEnd) {
+  //checks conditions like negatives, nonnumber, highEnd first.
+  if (!Number.isInteger(lowEnd) || !Number.isInteger(highEnd)) return "ERROR";
+  if (lowEnd < 0 || highEnd < 0) return "ERROR";
+  if (lowEnd > highEnd) {
+    const temporary = lowEnd;
+    lowEnd = highEnd;
+    highEnd = temporary;
+  }
+  const range = [];
+  for (let i = lowEnd; i <= highEnd; i++) {
+    range.push(i);
+  }
+  const sum = range.reduce((accu, curr) => accu + curr);
+  return sum;
 };
+sumAll(1, 5);
+// console.log(sumAll(0,0));
 
 // Do not edit below this line
 module.exports = sumAll;
