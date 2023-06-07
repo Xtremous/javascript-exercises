74df63a4cf5d720f3c32b91286baf5c7d137d2ec
Mohan <mohammadhashan99@gmail.com>
2023-06-07 13:42:01 -0400

    regex to replace specials then split,reverse,join compare

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  //get rid of special characters and uppercase
+  const processedString = str.toLowerCase().replace(/[^a-z]/g, "");
+  //return split reverse join compare
+  return processedString.split("").reverse().join("") == processedString;
 };
+// console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
