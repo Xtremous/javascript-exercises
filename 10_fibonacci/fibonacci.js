d0c19b0795d99dc0aebcd1169de496ef8a2ac018
Mohan <mohammadhashan99@gmail.com>
2023-06-07 14:30:42 -0400

    09 solved,google solution is cleaner

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (fib) {
+  if (fib < 0) return "OOPS";
+  let sequence = [1, 1];
+  for (let i = 0; i < fib - 2; i++) {
+    sequence.push(sequence[i] + sequence[i + 1]);
+  }
+  console.log(sequence);
+  return sequence.pop();
 };
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
