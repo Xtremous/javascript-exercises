b306aafceeb2447303567b15b711709a86654980
Mohan <mohammadhashan99@gmail.com>
2023-06-07 15:47:29 -0400

    12 find oldest, thanks google. try again

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+// console.log(Date());
+const findTheOldest = function (people) {
+  const thisYear = new Date().getFullYear();
 
+  return people.reduce(
+    (res, person) => {
+      const age = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+      return age > res.age ? { person, age } : res;
+    },
+    { person: null, age: 0 }
+  ).person;
 };
+
+// console.log(findTheOldest(people)); // Ray
 
 // Do not edit below this line
 module.exports = findTheOldest;
